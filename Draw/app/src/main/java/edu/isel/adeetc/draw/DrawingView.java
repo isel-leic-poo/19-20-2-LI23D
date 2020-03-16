fcda3d7a8a5f565e808d60b3a7dab54b3852be76
Paulo Pereira <palbp.giga@gmail.com>
2020-03-16 10:57:21 +0000

    Completa a implementação da aplicação Draw

--- Draw/app/src/main/java/edu/isel/adeetc/draw/DrawingView.java
+++ Draw/app/src/main/java/edu/isel/adeetc/draw/DrawingView.java
@@ -4,15 +4,22 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * View that displays the drawing area and its contents.
  */
-public class DrawingView extends View {
+public class DrawingView extends View implements View.OnTouchListener {
 
     /** The brush that will be used to paint. */
     private final Paint brush;
+    /** The view's model */
+    private final Drawing drawing;
+
+    private FreeStyleLine currentLine;
 
     /**
      * Initializes an instance with the given context (e.g. the hosting Activity)
@@ -24,6 +31,10 @@ public class DrawingView extends View {
         brush.setColor(Color.RED);
         brush.setStrokeWidth(10);
         brush.setStyle(Paint.Style.STROKE);
+
+        drawing = new Drawing();
+        setOnTouchListener(this);
+        currentLine = null;
     }
 
     /**
@@ -33,6 +44,50 @@ public class DrawingView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawLine(0, 0, getWidth(), getHeight(), brush);
+
+        for(FreeStyleLine line : drawing.getLines())
+            drawFreeStyleLine(canvas, line);
     }
+
+    /**
+     * Draws the given line in the given canvas
+     * @param canvas
+     * @param line
+     */
+    private void drawFreeStyleLine(Canvas canvas, FreeStyleLine line) {
+
+        final Point[] points = line.getPoints();
+        if(points.length == 0)
+            return;
+
+        Point startPoint = points[0];
+        for (int idx = 1; idx < points.length; ++idx) {
+            Point endPoint = points[idx];
+            canvas.drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y, brush);
+            startPoint = endPoint;
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.v("DrawingApp", "ACTION_DOWN");
+                currentLine = new FreeStyleLine();
+                drawing.add(currentLine);
+                return true;
+
+            case MotionEvent.ACTION_UP:
+                Log.v("DrawingApp", "ACTION_UP");
+                currentLine = null;
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                currentLine.addPoint(new Point((int) event.getX(), (int) event.getY()));
+                invalidate();
+
+        }
+        return false;
+    }
+
+
 }
