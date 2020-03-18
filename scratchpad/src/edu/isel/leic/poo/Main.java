0d4b3347d342640ad6ec48cd1ac1e1e0e2b79696
Paulo Pereira <palbp.giga@gmail.com>
2020-03-18 09:14:45 +0000

    Acrescenta as alterações realizadas ao projecto Scratchpad, usado para demonstração dos conceitos tratados

--- scratchpad/src/edu/isel/leic/poo/Main.java
+++ scratchpad/src/edu/isel/leic/poo/Main.java
@@ -1,60 +1,68 @@
 package edu.isel.leic.poo;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+interface Drawable {
+    void draw();
+}
+
+class Point implements Drawable {
+    final int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y +")";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println(toString());
+    }
+}
+
+class Point3D extends Point {
+    final int z;
+
+    Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+}
+
+class PsychedelicKitten implements Drawable {
+
+    @Override
+    public void draw() {
+        System.out.println("WOOOOOOOOOWOWOWOWOWOWOWOWOW");
+    }
+}
+
 
 public class Main {
 
-    private static ArrayList<Grade> approved(ArrayList<Grade> grades) {
-        return above(grades, new Grade(9));
-    }
-
-    private static ArrayList<Grade> below(ArrayList<Grade> grades, Grade reference) {
-        ArrayList<Grade> above = new ArrayList<>();
-        for (Grade grade : grades) {
-            if(grade.value < reference.value)
-                above.add(grade);
+    private static void drawAll(Drawable[] drawables) {
+        for (Drawable d : drawables) {
+            d.draw();
         }
-        return above;
-    }
-
-    private static ArrayList<Grade> above(ArrayList<Grade> grades, Grade reference) {
-        final ArrayList<Grade> above = new ArrayList<>();
-        for (Grade grade : grades) {
-            if(grade.value > reference.value)
-                above.add(grade);
-        }
-        return above;
-    }
-
-    private static Grade average(ArrayList<Grade> grades) {
-        int total = 0;
-        for (Grade grade : grades) {
-            total += grade.value;
-        }
-        return new Grade(total / grades.size());
     }
 
     public static void main(String[] args) {
-        final Scanner kbd = new Scanner(System.in);
-        final ArrayList<Grade> grades = new ArrayList<>();
-        int gradeValue;
-        while ((gradeValue = kbd.nextInt()) >= 0) {
-            final Grade grade = new Grade(gradeValue);
-            grades.add(grade);
-        }
 
-        final ArrayList<Grade> approved = approved(grades);
-        printGrades(approved);
-        final Grade average = average(grades);
-        System.out.println("Average = " + average.value);
-        final ArrayList<Grade> aboveAverage = above(grades, average);
-        printGrades(aboveAverage);
-    }
+        Point3D p = new Point3D(4, 5, 6);
+        p.draw();
 
-    private static void printGrades(ArrayList<Grade> grades) {
-        for (Grade grade : grades) {
-            System.out.println(grade.value);
-        }
+        final Drawable[] drawables = {
+                new PsychedelicKitten(), new Point(4, 6), new Point3D(12, 35, 6), new PsychedelicKitten()
+        };
+
+        drawAll(drawables);
     }
 }
