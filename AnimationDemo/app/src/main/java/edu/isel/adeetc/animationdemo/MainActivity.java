f92762b26dbceea00125d3bac2f933225a45e523
Paulo Pereira <palbp.giga@gmail.com>
2020-03-25 12:05:31 +0000

    Acrescenta a demo AnimationDemo, cujo código foi escrito durante a aula de 25/03/2020

---
+++ AnimationDemo/app/src/main/java/edu/isel/adeetc/animationdemo/MainActivity.java
@@ -0,0 +1,53 @@
+package edu.isel.adeetc.animationdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.TimeAnimator;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView totalTimeView = findViewById(R.id.totalTime);
+        final TextView partialTimeView = findViewById(R.id.partialTime);
+
+        final TimeAnimator animator = new TimeAnimator();
+        animator.setTimeListener(new TimeAnimator.TimeListener() {
+            @Override
+            public void onTimeUpdate(TimeAnimator animation, long totalTime, long deltaTime) {
+                totalTimeView.setText(Long.toString(totalTime));
+                partialTimeView.setText(Long.toString(deltaTime));
+            }
+        });
+
+        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animator.start();
+            }
+        });
+
+        final Button pauseButton = findViewById(R.id.pauseButton);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (animator.isPaused()) {
+                    pauseButton.setText("Paused");
+                    animator.resume();
+                }
+                else {
+                    pauseButton.setText("Resume");
+                    animator.pause();
+                }
+            }
+        });
+    }
+}
